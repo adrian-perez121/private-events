9abe495513483b45312642ccb00bec58c78df7cb
Adrian Perez <adrian1804perez@gmail.com>
2024-07-27 15:34:53 -0700

    Add title to Events table (oopsie)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_26_202727) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_27_223354) do
   create_table "events", force: :cascade do |t|
     t.string "location"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
