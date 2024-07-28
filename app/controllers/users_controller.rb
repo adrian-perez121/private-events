49ef4fb48ba89e99fb954d79871371b7286054f4
Adrian Perez <adrian1804perez@gmail.com>
2024-07-28 16:26:05 -0700

    Refactoring for loading in events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @events = Event.where(creator_id: params[:id].to_i).all.includes(:creator)
   end
 end
