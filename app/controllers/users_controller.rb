98b83f22fcdeac70ecf4f0cb4d31df9ed44c0688
Adrian Perez <adrian1804perez@gmail.com>
2024-07-29 12:31:21 -0700

    Add attended events to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @events = Event.where(creator_id: params[:id].to_i).all.includes(:creator)
+    @attended_events = @user.attended_events
   end
 end
